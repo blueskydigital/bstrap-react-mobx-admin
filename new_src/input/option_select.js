fcd12c8c8c0cc58c240b0772dd2af69c11ec9211
Adam Jun <adam@bluesky.pro>
2020-10-12 11:31:52 +0200

    feature: OptionSelect with new parameters

--- new_src/input/option_select.js
+++ new_src/input/option_select.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {observer} from 'mobx-react'
-import {Radio, FormGroup, ControlLabel, HelpBlock} from 'react-bootstrap'
+import { observer } from 'mobx-react'
+import { Radio, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap'
 
 const OptionSelect = ({
   attr, record, onChange, errors, label, disabled, options,
+  labelattr = 'label', valueattr = 'value', value = null,
   extractOpt = (i) => i
 }) => {
-  const val = record.get(attr) ? record.get(attr).toString() : null
+  const val = value || (attr && record.get(attr) ? record.get(attr).toString() : null)
   const errorText = errors.has(attr) ? errors.get(attr) : undefined
   const validationState = errorText ? 'error' : null
 
@@ -17,13 +18,13 @@ const OptionSelect = ({
       <div>
         {
           options.map((i, idx) => {
+            const isChecked = val === i[valueattr]
             return (
-              <Radio key={idx} name={attr} checked={val === i.value}
-                disabled={disabled} inline
-                onChange={() => {
-                  onChange(attr, i.value)
-                }}>
-                {i.label}
+              <Radio key={idx} name={attr} checked={isChecked} inline
+                disabled={disabled} className={isChecked && 'text-primary text-bold'}
+                style={{ marginLeft: '0', marginRight: '1em' }}
+                onChange={_ => { onChange(attr, i[valueattr]) }}>
+                {i[labelattr]}
               </Radio>
             )
           })
