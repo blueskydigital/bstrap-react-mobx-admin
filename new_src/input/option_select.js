dc99649f5e808c229f083ea601dcd4ca8d745d25
Adam Jun <adam@bluesky.pro>
2020-10-13 16:26:29 +0200

    feature: Option select input with optionsattr, optionsrecord and new class for checked

--- new_src/input/option_select.js
+++ new_src/input/option_select.js
@@ -4,31 +4,34 @@ import { observer } from 'mobx-react'
 import { Radio, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap'
 
 const OptionSelect = ({
-  attr, record, onChange, errors, label, disabled, options,
+  attr, record, onChange, errors, label, disabled, options, optionsattr, optionsrecord,
   labelattr = 'label', valueattr = 'value', value = null,
   extractOpt = (i) => i
 }) => {
   const val = value || (attr && record.get(attr) ? record.get(attr).toString() : null)
-  const errorText = errors.has(attr) ? errors.get(attr) : undefined
+  const errorText = errors && errors.has(attr) ? errors.get(attr) : undefined
   const validationState = errorText ? 'error' : null
+
+  options = options || (
+    optionsattr && optionsrecord &&
+    optionsrecord.has(optionsattr) &&
+    optionsrecord.get(optionsattr))
 
   return (
     <FormGroup validationState={validationState}>
       <ControlLabel>{label}</ControlLabel>
       <div>
-        {
-          options.map((i, idx) => {
-            const isChecked = val === i[valueattr]
-            return (
-              <Radio key={idx} name={attr} checked={isChecked} inline
-                disabled={disabled} className={isChecked && 'text-primary text-bold'}
-                style={{ marginLeft: '0', marginRight: '1em' }}
-                onChange={_ => { onChange(attr, i[valueattr]) }}>
-                {i[labelattr]}
-              </Radio>
-            )
-          })
-        }
+        {options.map((i, idx) => {
+          const isChecked = val === i[valueattr]
+          return (
+            <Radio key={idx} name={attr} checked={isChecked} inline
+              disabled={disabled} className={isChecked && 'text-primary text-bold'}
+              style={{ marginLeft: '0', marginRight: '1em' }}
+              onChange={_ => { onChange(attr, i[valueattr]) }}>
+              {i[labelattr]}
+            </Radio>
+          )
+        })}
       </div>
       {errorText ? <HelpBlock>{errorText}</HelpBlock> : null}
     </FormGroup>
@@ -37,10 +40,11 @@ const OptionSelect = ({
 OptionSelect.propTypes = {
   attr: PropTypes.string.isRequired,
   errors: PropTypes.object,
+  extractOpt: PropTypes.func,
   label: PropTypes.string,
   onChange: PropTypes.func.isRequired,
-  record: PropTypes.object.isRequired,
   options: PropTypes.array.isRequired,
-  extractOpt: PropTypes.func
+  record: PropTypes.object.isRequired
 }
+
 export default observer(OptionSelect)
